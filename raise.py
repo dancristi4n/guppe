c06c8e2f492cf77cdef4a1aa4c7ee069f70428d4
Danilo Soares <danilo.ndos007@gmail.com>
2022-10-04 22:00:31 -0300

    add raise and commons errors

---
+++ raise.py
@@ -0,0 +1,69 @@
+"""
+Levantando os próprios erros com raise
+
+raise -> Lança exceções
+
+OBS: O raise não é uma função. É uma palavra reservada, assim como def ou qualquer outra em Python.
+
+Para simplicar, pense no raise como sendo útil par que possamos criar nossas próprias exceções e mensagens de erro.
+
+A forma geral de utilização é:
+
+raise TipoDoErro('Mensagem de erro')
+
+# Exemplo real
+
+def colore(texto, cor):
+    if type(texto) is not str:
+        raise TypeError('Texto precisa ser uma string')
+
+    if type(cor) is not str:
+        raise TypeError('cor precisa ser uma string')
+
+    print(f'O texto {texto} será impresso na cor {cor}')
+
+
+colore('Geek', 7)
+
+
+# Exemplo refatorado
+
+def colore(texto, cor):
+    cores = ('verde', 'amarelo', 'azul', 'branco')
+    if type(texto) is not str:
+        raise TypeError('Texto precisa ser uma string')
+
+    if type(cor) is not str:
+        raise TypeError('cor precisa ser uma string')
+
+    if cor not in cores:
+        raise ValueError(f'A cor precisa ser uma entre: {cores}')
+
+    print(f'O texto {texto} será impresso na cor {cor}')
+
+
+colore('Geek University', 'preto')
+
+OBS: O raise, assim como o return, finaliza a função. Ou seja, nada após o raise é executado.
+
+"""
+
+# Exemplo real
+
+def colore(texto, cor):
+    cores = ('verde', 'amarelo', 'azul', 'branco')
+    if type(texto) is not str:
+        raise TypeError('Texto precisa ser uma string')
+
+    if type(cor) is not str:
+        raise TypeError('cor precisa ser uma string')
+
+    if cor not in cores:
+        raise ValueError(f'A cor precisa ser uma entre: {cores}')
+
+    print(f'O texto {texto} será impresso na cor {cor}')
+
+
+colore('Geek University', 'preto')
+
+
