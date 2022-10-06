f67da96f2674b3262030d02cf1b62afe60191c2f
Danilo Soares <danilo.ndos007@gmail.com>
2022-10-06 09:01:24 -0300

    try_except

---
+++ try_except.py
@@ -0,0 +1,57 @@
+"""
+O block try/except
+
+Utilizamos o bloco try/except para tratar erros que podem ocorrer no nosso código. Previnindo assim que o programa pare de funcionar e o usuário receba mensagens de erro inesperadas.
+
+A forma geral mais simples é:
+
+try:
+    //execução problemática
+
+except:
+    //o que deve ser feito em caso de problema
+
+# Exemplo 1 - Tratando um erro genérico
+
+try:
+    geek()
+
+except:
+    print('Deu algum problema')
+
+# Tente executar a função geek(), caso você encontre erros, imprima a mensagem de erro
+
+# Exemplo 2 - Tratando um erro genérico
+
+try:
+    len(5)
+
+except:
+    print('Deu algum problema')
+
+# Tente executar a função geek(), caso você encontre erros, imprima a mensagem de erro
+
+OBS: Tratar erro de forma genérica não é a melhor forma de tratamento de erros. O ideal é SEMPRE tratar de forma específica.
+
+# Exemplo 3 - Tratando um erro específico
+
+try:
+    geek()
+
+except NameError:
+    print('Você está utilizando uma função inexistente')
+
+# Tente executar a função geek(), caso você encontre erros, imprima a mensagem de erro
+
+"""
+
+# Exemplo 3 - Tratando um erro específico
+
+try:
+    len(5)
+
+except NameError:
+    print('Você está utilizando uma função inexistente')
+
+# Tente executar a função geek(), caso você encontre erros, imprima a mensagem de erro
+
