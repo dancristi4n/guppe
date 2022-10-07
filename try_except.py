7cbba739f579b2e6150a4154f24358074442164a
Danilo Soares <danilo.ndos007@gmail.com>
2022-10-06 22:42:10 -0300

    try, except, else, finally and debugging

--- try_except.py
+++ try_except.py
@@ -43,15 +43,44 @@ except NameError:
 
 # Tente executar a função geek(), caso você encontre erros, imprima a mensagem de erro
 
-"""
-
-# Exemplo 3 - Tratando um erro específico
+# Exemplo 4 - Tratando um erro específico
 
 try:
     len(5)
 
-except NameError:
+except TypeError:
     print('Você está utilizando uma função inexistente')
 
-# Tente executar a função geek(), caso você encontre erros, imprima a mensagem de erro
+# Exemplo 5 - Tratando um erro específico com detalhes do erro
 
+try:
+    len(5)
+
+except TypeError as err:
+    print(f'A aplicação gerou o seguinte erro: {err}')
+
+# Podemos efetuar diversos tratamentos de erros de uma vez
+
+try:
+    #len(5)
+    geek()
+    #print('Geek'[9])
+except NameError as erra:
+    print(f'Deu NameError: {erra}')
+except TypeError as errb:
+    print(f'Deu TypeError: {errb}')
+except:
+    print('Deu um erro diferente')
+"""
+
+def pega_valor(dicionario, chave):
+    try:
+        return dicionario[chave]
+    except KeyError:
+        return None
+    except TypeError:
+        return None
+
+dic = {'nome': 'Geek'}
+
+print(pega_valor(dic, 8))
