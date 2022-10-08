22c43f6f09e6713270b729a412c45b79ea4922d8
Danilo Soares <danilo.ndos007@gmail.com>
2022-10-08 20:55:19 -0300

    mod external

--- modulos_externos.py
+++ modulos_externos.py
@@ -30,7 +30,7 @@ print(Style.DIM + 'Geek University')
 
 import pydf
 
-pdf = pydf.generate_pdf('<h1>Geek University</h1>')
+pdf = pydf.generate_pdf('<h1>Geek University</h1></br></br><strong>Programa&ccedil;&atilde;o em Python: Essencial</strong>')
 
 with open('test_doc.pdf', 'wb') as f:
     f.write(pdf)
