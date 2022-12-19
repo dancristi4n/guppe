23e16b84c8c980d78b3629d83b58762177b9b348
Danilo Soares <danilo.ndos007@gmail.com>
2022-12-19 00:03:56 -0300

    add novidades 3.8, operador walrus e argumentos somente posicionais

---
+++ operador_walrus.py
@@ -0,0 +1,21 @@
+"""
+O operador Walrus permite fazer a atribuição e retorno de valor em uma única expressão.
+
+variável := expressão
+"""
+# nome = 'Geek University'
+# print(nome)
+#
+# print(nome := 'Geek University')
+
+# Python 3.7
+# cesta = []
+# fruta = input('Informe a fruta: ')
+# while fruta != 'jaca':
+#     cesta.append(fruta)
+#     fruta = input('Informe a fruta: ')
+
+# Python 3.8
+cesta = []
+while (fruta := input('Informe a fruta: ')) != 'jaca':
+    cesta.append(fruta)
