ef381eb86ae0ba427162d519e49bf3d7255e7832
Danilo Soares <danilo.ndos007@gmail.com>
2022-12-19 22:10:42 -0300

    add tipos de dados mais precisos e debugger fstrings

---
+++ debugger_fstrings.py
@@ -0,0 +1,20 @@
+# def multiplicar(num1: float, num2: float) -> float:
+#     return num1 * num2
+#
+#
+# resultado: float = multiplicar(4.2345, 6.7898)
+#
+# print(f'O resultado é {resultado}')
+#
+# print(f'O resultado é {multiplicar(9, 4):.2f}')
+#
+# print(f'{(media := 8 / 2)} é a metade de {media * 2}')
+
+geek: str = 'Geek Universirty'
+
+print(f"geek='{geek}'")
+
+print(f'{geek=}')
+
+print(f'{geek.upper()[::-1] = }')
+# geek.upper()[::-1] = 'YTRISREVINU KEEG'
