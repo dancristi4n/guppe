0420ca409b722bf9db7430ce6186fc0de4851968
Danilo Soares <danilo.ndos007@gmail.com>
2023-01-03 22:02:11 -0300

    add sintaxes perigosas

---
+++ sintaxes_perigosas.py
@@ -0,0 +1,14 @@
+"""
+
+== -> é usando para checar valor
+
+is -> é usado para chegar se os objetos são os mesmos
+
+versao = '3.8'
+versao is '3.8'
+<input>:1: SyntaxWarning: "is" with a literal. Did you mean "=="?
+<input>:1: SyntaxWarning: "is" with a literal. Did you mean "=="?
+False
+versao == '3.8'
+True
+"""
